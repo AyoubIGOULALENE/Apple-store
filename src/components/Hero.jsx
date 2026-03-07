a5fc79c7aab930087dc362059eccf40fb7940603
Ayoub-IG <ihayoub601@gmail.com>
2026-03-07 16:27:12 +0100

    adding hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section id='hero'>
+        <h1>MacBook Pro</h1>
+        <img src="/title.png" alt="MacBook Pro" srcset="" />
+        <video src="/videos/hero.mp4" autoPlay playsInline muted></video>
+    <button >Buy me</button>
+    <p>from $1,999 to $2,499</p>
+    </section>
+  )
+}
+
+export default Hero
