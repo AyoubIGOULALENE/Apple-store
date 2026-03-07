69aedf5f0befc25ca5f0d7165f8f22a264bc9d0c
Ayoub-IG <ihayoub601@gmail.com>
2026-03-07 16:13:30 +0100

    Add React project files

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Navlinks from '../constants'
+const Navbar = () => {
+  return (
+    <header>
+        <nav>
+            <img src="/logo.svg" alt="apple logo" srcset="" />
+            <ul>
+                {
+                    Navlinks.map(({ name, link }) => (
+                        <li key={name}>
+                            <a href={link}>{name}</a>
+                        </li>
+                    ))  
+                }
+            </ul>
+            <div className='flex-center gap-3'>
+                <button>
+                    <img src="/search.svg" alt="search icon" srcset="" />
+                </button>
+                <button>
+                    <img src="/cart.svg" alt="bag icon" srcset="" />
+                </button>
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
---
+++ src/constants/index.js
@@ -0,0 +1,7 @@
+const Navlinks = [   { name: 'Mac', link: '#' },
+                        { name: 'iPad', link: '#' },
+                        { name: 'iPhone', link: '#' },
+                        { name: 'Watch', link: '#' },
+                        { name: 'TV', link: '#' },
+                        { name: 'Music', link: '#' },]
+export default Navlinks
